4870e71b7aeac67f2546c5d3fd14487dde85f20b
Julio <38119079+Juliodonadello@users.noreply.github.com>
2021-08-29 19:06:08 -0300

    Delete Notas/02_Estructuras_y_Funciones/clase2 directory

--- Notas/02_Estructuras_y_Funciones/clase2/2.3.py
+++
@@ -1,9 +0,0 @@
-#2.3
-with open("Data\precios.csv","rt") as f:
-    for line in f:
-        row = line.split(",")
-        if row[0] == "Naranja":
-            print(f"El precio de la Naranja es: {row[1]}")
-            break
- 
-            
